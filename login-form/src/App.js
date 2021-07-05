f2f26324a213f9007d3b20331e4680c58c95e216
Sashminda Withanage <sashmindaps4@gmail.com>
2021-07-05 16:30:55 +0530

    Cleaned up and started implementation

--- login-form/src/App.js
+++ login-form/src/App.js
@@ -1,24 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useTheme, ThemeProvider, withTheme } from "@emotion/react";
+import theme from "@rebass/preset";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className="App">test</div>
+    </ThemeProvider>
   );
 }
 
