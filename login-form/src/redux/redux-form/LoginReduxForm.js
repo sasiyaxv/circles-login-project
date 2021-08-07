2acb0d2a8061a91a18b92951d15f82840d6be4bd
sashminda iranga <sashmindaps4@gmail.com>
2021-08-07 08:22:09 +0530

    Updated

--- login-form/src/redux/redux-form/LoginReduxForm.js
+++ login-form/src/redux/redux-form/LoginReduxForm.js
@@ -2,9 +2,21 @@ import React from "react";
 import { Field, reduxForm } from "redux-form";
 
 let LoginReduxForm = ({ handleSubmit }) => {
+  const validations = (values) => {
+    const errors = {};
+    if (!values.userName) {
+      errors.userName = "Required";
+    } else if (values.userName.length > 15) {
+      errors.userName = "Must be 15 characters or less";
+    }
+    console.log("ERR", errors);
+    return errors;
+  };
+
   function handleSubmit(values) {
     values.preventDefault();
     console.log(values);
+    validations(values);
   }
   return (
     <form onSubmit={handleSubmit}>
